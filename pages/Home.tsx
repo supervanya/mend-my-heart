b39795f8766b579ddce50b61f5e47df13819e7e4
Vanya Prokopovich <iprokopovich@circadianrisk.com>
2023-05-03 10:05:37 -0700

    cleanup

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -26,16 +26,13 @@ const getResponseFromBot = async (
   return data;
 };
 
-const lastActiveChat: TPersonas = "lifeCoach";
-
 export default function Home() {
   const chatRef = useRef<HTMLDivElement>(null);
   const [fetching, setFetching] = useState(false);
   const [input, setInput] = useState<string>("");
   const { lastActiveChat: persona, setLastActiveChat } = useLastActiveChat();
-  // const [persona, setPersona] = useState<TPersonas>(lastActiveChat);
 
-  const { greeting } = PROMPTS[persona];
+  const { greeting, oneLiner } = PROMPTS[persona];
 
   const { chatHistory, addMessageToHistory, resetChatHistory } =
     useMessages(persona);
@@ -77,11 +74,6 @@ export default function Home() {
     scrollToBottom();
   };
 
-  const handleSetPersona = (persona: TPersonas) => {
-    setLastActiveChat(persona);
-    // setPersona(persona);
-  };
-
   return (
     <>
       <Head>
@@ -92,9 +84,10 @@ export default function Home() {
         <nav className="sticky top-0 z-10 w-full rounded-sm bg-slate-600  px-8 py-4 text-center  text-white backdrop-blur-3xl">
           <div className="flex flex-col items-center gap-2">
             <select
+              value={persona}
               className="rounded-md bg-slate-700 p-1 text-white"
               onChange={({ target }) =>
-                handleSetPersona(target.value as TPersonas)
+                setLastActiveChat(target.value as TPersonas)
               }
             >
               {toPairs(PROMPTS).map(([persona, settings]) => (
@@ -103,6 +96,8 @@ export default function Home() {
                 </option>
               ))}
             </select>
+
+            <span className="text-base text-slate-100">{oneLiner}</span>
 
             <span className="text-xs text-slate-300">
               Remember, I am just a silly robot
