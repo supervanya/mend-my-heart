d349ec010ff8a7e0af49957d8d40eafaed9e5055
Vanya Prokopovich <iprokopovich@circadianrisk.com>
2023-05-11 13:43:23 -0700

    running migration

--- helpers/useDb.tsx
+++ helpers/useDb.tsx
@@ -1,12 +1,46 @@
 import { db } from "@/db";
 import { TPersonas } from "./constants";
-import { TMessage } from "./types";
+import { TChatHistory, TMessage } from "./types";
 import { useEffectOnce } from "react-use";
 import { useLiveQuery } from "dexie-react-hooks";
+import { isArray } from "lodash";
 
 const runMigrations = () => {
   // this was used with the initial release of the app
-  console.warn("implement migrations");
+  const HAS_MIGRATED_KEY = "hasMigrated";
+  const hasMigrated = localStorage.getItem(HAS_MIGRATED_KEY);
+  if (hasMigrated !== null) {
+    return;
+  }
+
+  const OLD_KEY = "chatHistory";
+  const OLD_PERSONA: TPersonas = "relationshipTherapist";
+
+  try {
+    const oldRelationshipChatHistory = localStorage.getItem(OLD_KEY);
+
+    if (oldRelationshipChatHistory) {
+      let parsed = JSON.parse(oldRelationshipChatHistory);
+
+      if (!isArray(parsed)) {
+        parsed = JSON.parse(parsed);
+      }
+
+      if (isArray(parsed)) {
+        console.warn("Migrating old chat history to new format", parsed);
+        (parsed as TChatHistory).forEach((message) => {
+          db.messages.add({ ...message, persona: OLD_PERSONA });
+        });
+
+        localStorage.setItem(HAS_MIGRATED_KEY, new Date().toISOString());
+        console.warn("Migration successful");
+
+        // localStorage.removeItem(OLD_KEY);
+      }
+    }
+  } catch (error) {
+    console.error("Error migrating old chat history", error);
+  }
 };
 
 export const useDb = (persona: TPersonas) => {
